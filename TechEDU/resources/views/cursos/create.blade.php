1d4b2f544b01c6526727ad42dbb7d112cffe2bcb
AlissaGarcia <alissa.garcia08@aluno.ifce.edu.br>
2026-06-15 02:18:24 +0000

    criação do forrmulário de curso com os campos nome e carga horária, além do botão de cadastro, para exemplificar melhor.

--- TechEDU/resources/views/cursos/create.blade.php
+++ TechEDU/resources/views/cursos/create.blade.php
@@ -5,8 +5,15 @@
 </head>
 <body>
     <h1>{{ $titulo }}</h1>
-    <form action="/cursos" method="GET">
-        <p>Este formulário é apenas para demonstrar a view de cadastro.</p>
+    <!-- Formulário de exemplo simples criado apenas para demonstrar a estrutura -->
+    <form action="#" method="POST">
+        <label for="nome">Nome do curso:</label>
+        <input type="text" id="nome" name="nome" placeholder="Digite o nome" required>
+
+        <label for="carga_horaria">Carga horária:</label>
+        <input type="number" id="carga_horaria" name="carga_horaria" placeholder="Horas" required>
+
+        <button type="submit">Cadastrar</button>
     </form>
 </body>
 </html>
